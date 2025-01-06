42467771bf2c75018bfa436a773ade7e09298324
edwardalx <eobkwaku@gmail.com>
2025-01-06 08:21:50 +0000

    v3

--- Loops/forLoopIn_a_Range.js
+++ Loops/forLoopIn_a_Range.js
@@ -1,7 +1,7 @@
 let number=""
-for(i=1; i<8; i++){
+for(i=1; i<5; i++){
       console.log(i);
- let numbers = i;
- number = number + numbers +"\n"
+ //let numbers = i;   we nomally do this lie inside a bracket whe we want to iterate with for (let numbers of i)
+ number = number + i
 }
 console.log(number);  
---
+++ Loops/whileLoop.js
@@ -0,0 +1,6 @@
+let x = 1
+while(x <= 5){
+ x+=1
+ console.log(x)
+}
+//  console.log(x)    // should always be inside th e while lopp block
